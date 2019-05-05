56b069659e733e2afc3179d544b47d690e9522f8
LuoPK <luopk@ikangtai.com>
2019-05-05 20:13:26 +0800

    JavaScriptCoreDemo 新增 WebViewController

---
+++ JavaScriptCoreDemo/JavaScriptCoreDemo/WebViewController.h
@@ -0,0 +1,17 @@
+//
+//  WebViewController.h
+//  JavaScriptCoreDemo
+//
+//  Created by mac on 2019/5/5.
+//  Copyright © 2019 ikangtai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
